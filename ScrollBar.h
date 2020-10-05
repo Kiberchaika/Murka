950a50fde4c34f7b406cb6c607f8aedcf3e91a45
Alexey Popov <yeswecan8@yandex.ru>
2020-10-05 14:08:21 +0300

    Horizontal scrollbar added

--- ScrollBar.h
+++ ScrollBar.h
@@ -17,7 +17,7 @@ public:
         auto results = (Results*)resultObject;
         bool inside = context.isHovered() * !areInteractiveChildrenHovered(context) * hasMouseFocus(context);
 
-        if (params->kind == 0) {
+        if (params->kind == 0) { // Vertical scrollbar
             if (params->surfaceToControl->panLimits.size.y > 0) {
 
                float scrollbarHeight = (context.getSize().y / (params->surfaceToControl->panLimits.size.y + context.getSize().y)) * context.getSize().y;
@@ -37,6 +37,42 @@ public:
                     params->surfaceToControl->panOffset.y -= (lastFrameMousePosition.y - context.mousePosition.y) * (context.getSize().y / scrollbarHeight);
                     if (params->surfaceToControl->panOffset.y < 0) params->surfaceToControl->panOffset.y = 0;
                     if (params->surfaceToControl->panOffset.y > params->surfaceToControl->panLimits.size.y) params->surfaceToControl->panOffset.y = params->surfaceToControl->panLimits.size.y;
+
+                }
+                
+                if ((handleShape.inside(context.mousePosition)) && inside && context.mouseDownPressed[0]) {
+                    draggingNow = true;
+                }
+
+                if ((draggingNow) && (!context.mouseDown[0])) {
+                    draggingNow = false;
+                }
+
+                lastFrameMousePosition = context.mousePosition;
+
+            }
+        }
+        
+        if (params->kind == 1) { // Horizontal scrollbar
+            if (params->surfaceToControl->panLimits.size.x > 0) {
+
+               float scrollbarWidth = (context.getSize().x / (params->surfaceToControl->panLimits.size.x + context.getSize().x)) * context.getSize().x;
+                
+               MurkaShape surfaceShape = ((View*)params->surfaceToControl)->shape;
+                
+               MurkaShape handleShape = {(context.getSize().x - scrollbarWidth) *
+                                            (params->surfaceToControl->panOffset.x / (params->surfaceToControl->panLimits.size.x)),
+                                            0,
+                                            scrollbarWidth,
+                                            context.getSize().x};
+               MurkaShape scrollbarShape = {0, 0, context.getSize().x, context.getSize().y};
+                
+               params->drawScrollBar(context, scrollbarShape, handleShape, handleShape.inside(context.mousePosition), false);
+                
+                if (draggingNow) {
+                    params->surfaceToControl->panOffset.x += (lastFrameMousePosition.x - context.mousePosition.x) * (context.getSize().x / scrollbarWidth);
+                    if (params->surfaceToControl->panOffset.x < 0) params->surfaceToControl->panOffset.x = 0;
+                    if (params->surfaceToControl->panOffset.x > params->surfaceToControl->panLimits.size.x) params->surfaceToControl->panOffset.x = params->surfaceToControl->panLimits.size.x;
 
                 }
                 
